a616e2e6a7830df442bc2aab539926c299243739
NbsSahitya <sahityasingh@cedcoss.com>
2022-02-05 13:36:23 +0530

    Task43:-Access Multi-Dimensional Arrays With Indexes

--- src/index.js
+++ src/index.js
@@ -147,3 +147,11 @@ const myArray = [18, 64, 99]; //Task42:-Modify Array Data With Indexes
 myArray[0]=45;
 myArray[1]=64;
 myArray[2]=99;
+const myArray = [
+      [1, 2, 3],   
+      [4, 5, 6],
+      [7, 8, 9],
+      [[10, 11, 12], 13, 14],
+    ];
+  
+    const myData = myArray[2][1]; //Task43:-Access Multi-Dimensional Arrays With Indexes
