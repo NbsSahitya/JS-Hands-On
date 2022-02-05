33c82874e3240bbcf0a9e967c5a21c39980d8f0d
NbsSahitya <sahityasingh@cedcoss.com>
2022-02-05 11:18:52 +0530

    Task11:-Add Two Numbers with JavaScript

--- src/index.js
+++ src/index.js
@@ -39,3 +39,4 @@ const FCC = "freeCodeCamp"; // Change this line //Task10:-Declare a Read-Only Va
 let fact = "is cool! "; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+const sum = 10 + 10; //Task11:-Add Two Numbers with JavaScript
