7c8071c5454733b584a1ead342ff1904035a0dfc
NbsSahitya <sahityasingh@cedcoss.com>
2022-02-05 14:44:17 +0530

    Task52:-Global Scope and Functions

--- src/index.js
+++ src/index.js
@@ -189,3 +189,22 @@ function timesFive(a){ //Task51:-Return a Value from a Function with Return
     timesFive(5);
     timesFive(2);
     timesFive(0);
+    let myGlobal=10; //Task52:-Global Scope and Functions
+let oopsGlobal;
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal=5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
