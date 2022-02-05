35296816b70a9ab5fba5e6d4ead6926f00a83ec6
NbsSahitya <sahityasingh@cedcoss.com>
2022-02-05 10:38:00 +0530

    Task4:- Assigning the Value of One Variable to Another

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 /* This is a
 multi-line comment */  //Task1 :-Comment Your JavaScript Code
 var myName; //Task2:- Declare JavaScript Variables
+var a;
+a = 7; // Task3:- Storing Values with the Assignment Operator
+var a; // Task4:- Assigning the Value of One Variable to Another
+a = 7;
+var b;
+b=7;
+b = a;
