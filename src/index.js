652f303d48aefae006b7bc8926d780b43bdb5b83
NbsSahitya <sahityasingh@cedcoss.com>
2022-02-05 10:59:01 +0530

    Task7:- Understanding Uninitialized Variables

--- src/index.js
+++ src/index.js
@@ -12,3 +12,15 @@ b = a;
 var a = 9; //Task5:-Initializing Variables with the Assignment Operator
 var myFirstName = "Sahitya"; //Task6:-Declare String Variables
 var myLastName = "Singh";
+// Only change code below this line
+var a;   
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a";
+// Only change code above this line
+
+a = a + 1;  //Task7:- Understanding Uninitialized Variables
+b = b + 5;
+c = c + " String!";
